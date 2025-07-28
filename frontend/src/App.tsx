f1d8be3cad2fb2238100b103f6dbb8a358b3917a
Hector <52350216+Higna@users.noreply.github.com>
2025-07-28 16:17:21 +0100

    update

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import Home from './components/home'
+import Signup from './components/signup'
+import Login from './components/login'
+import Navbar from './components/navbar'
+import {Routes, Route} from 'react-router-dom'
+
+export default function App() {
+
+  return (
+    <>
+    </>
+  )
+}
+
+
